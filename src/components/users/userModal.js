fa09382af384959e69c13bc8d1cdca183d7e126e
manasn <manasn@lanetteam.com>
2019-03-12 17:52:22 +0530

    first commit

---
+++ src/components/users/userModal.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Button, Modal, Form, FormGroup, Input, ModalFooter } from 'reactstrap';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+
+import * as userActions from '../../actions/user.action';
+
+export class UserModal extends Component {
+    state = {
+        id: 0,
+        name: '',
+        email: '',
+        gender: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    handleSubmit = () => {
+        if (!this.props.user) {
+            this.props.addUser({ ...this.state });
+        }
+        else {
+            let user = this.props.user;
+            user.name = this.state.name;
+            user.email = this.state.email;
+            user.gender = this.state.gender;
+            this.props.editUser(user.id, user)
+        }
+        this.closeModal();
+    }
+    closeModal = () => {
+        this.setState({ name: '', email: '', gender: '', id: 0 })
+        this.props.toggle();
+    }
+    componentWillReceiveProps = (props) => {
+        if (props.user) {
+            this.setState({ id: props.user.id, name: props.user.name, email: props.user.email, gender: props.user.gender })
+        }
+    }
+    render() {
+        return (
+            <Modal isOpen={this.props.isOpen} toggle={this.closeModal}>
+                <Form style={{ margin: '20px' }}>
+                    <FormGroup>
+                        <Input type='text' name='name' placeholder='Name' defaultValue={this.state.name} onChange={this.handleChange} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type='email' name='email' placeholder='Email' defaultValue={this.state.email} onChange={this.handleChange} />
+                    </FormGroup>
+                    Gender:
+                    <FormGroup check>
+                        <Input type='radio' name='gender' value="Male" onChange={this.handleChange} defaultChecked={this.state.gender === "Male"} />
+                        Male
+                    </FormGroup>
+                    <FormGroup check>
+                        <Input type='radio' name='gender' value="Female" onChange={this.handleChange} defaultChecked={this.state.gender === "Female"} />
+                        Female
+                    </FormGroup>
+                    <ModalFooter>
+                        {this.props.user ? <Button className='update' onClick={this.handleSubmit} color='info'>Update</Button> :
+                            <Button className='submit' onClick={this.handleSubmit} color='info'>Submit</Button>}
+                        <Button className='cancel' onClick={this.closeModal} color='danger'>Cancel</Button>
+                    </ModalFooter>
+                </Form>
+            </Modal>
+        )
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        addUser: bindActionCreators(userActions.addUserAction, dispatch),
+        editUser: bindActionCreators(userActions.editUserAction, dispatch)
+    }
+}
+
+export default connect(null, mapDispatch)(UserModal);
---
+++ src/components/users/users.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+
+import * as userActions from '../../actions/user.action';
+import UserModal from './userModal';
+
+export class Users extends Component {
+
+    state = {
+        showModal: false,
+        user: undefined
+    }
+
+    componentWillMount() {
+        this.props.getUsers();
+    }
+
+    toggle = (user) => {
+        this.setState(prevState => ({ showModal: !prevState.showModal, user }))
+    }
+    handleDelete = (id) => {
+        this.props.deleteUser(id);
+    }
+    render() {
+        return (
+            <div style={{ margin: '20px' }}>
+                <Button onClick={() => this.toggle(undefined)} color='info' className='add'>Add User</Button>
+                <UserModal isOpen={this.state.showModal} toggle={this.toggle} user={this.state.user} />
+                <Table dark style={{ textAlign: 'center', margin: '15px' }}>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Gender</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.users.map((user, i) => (
+                                <tr key={i}>
+                                    <td>{i + 1}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.gender}</td>
+                                    <td>
+                                        <Button onClick={() => this.toggle(user)} color='info' className='edit'>Edit</Button>{" "}
+                                        <Button onClick={() => this.handleDelete(user.id)} color='danger' className='delete'>Delete</Button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
+}
+
+const mapState = (state) => {
+    const { users } = state.user
+    return {
+        users
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        getUsers: bindActionCreators(userActions.getUsersAction, dispatch),
+        deleteUser: bindActionCreators(userActions.deleteUserAction, dispatch)
+    }
+}
+export default connect(mapState, mapDispatch)(Users);
